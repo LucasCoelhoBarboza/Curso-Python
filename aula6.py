6ecc269ba748cc013df71ba463f144f79e91d07a
LucasCoelhoBarboza <lucs.c0elh@gmail.com>
2024-11-25 17:59:23 -0300

    Somando número

---
+++ aula6.py
@@ -0,0 +1,13 @@
+
+
+# int - 7, -4, 0, 9875 (numeros inteiros)
+# floa - 4.5, 0.076, -15.223, 7.0 (numero reais)
+# bool - True, False
+# srt - 'olá', 7.5, ''
+
+n1 = int(input('digite um valor: '))
+n2 = int(input('digite mais valor: '))
+s= float(n1 + n2)
+# print('a some vale', s)
+
+print('a soma entre {} e {} vale {}'.format(n1, n2, s))
